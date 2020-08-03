1c794af9121d8708e0eac0b9276222323c5db716
Aryan Kini <aryankini@gmail.com>
2020-08-03 10:46:50 -0400

    Somewhat finished InstaBot

---
+++ instabot.py
@@ -0,0 +1,57 @@
+#Import Statements Needed For Program
+from selenium import webdriver
+import time
+import random
+
+#Status Variables
+instaOpen = False
+forever = True
+
+#User information
+username = "engagestemeastbrunswick"
+password = "engagestemeb421"
+
+#URL for Website
+url = "https://www.instagram.com/"
+while forever:
+    time.sleep(60)
+    #Open Browser and Open Up Instragram
+    try:
+        driver = webdriver.Chrome('C:/Users/aryan/Downloads/chromedriver.exe')
+        driver.get(url)
+        instaOpen = True
+    except:
+        print("Chromedriver Failure")
+
+    if(instaOpen):
+        #Log into Instrgram
+        driver.implicitly_wait(5)
+        driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input").send_keys(username) #username
+        driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input").send_keys(password) #password
+        driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button").click() #Log in Button
+
+        #Auto-Login Denied
+        driver.implicitly_wait(10)
+        try:
+            driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div/div/div/button").click()
+        except:
+            print("That option doesn't exsist")
+
+        #No Notifications
+        driver.implicitly_wait(5)
+        try:
+            driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[3]/button[2]").click()
+        except:
+            print("That option doesn't exsist")
+
+        #See All New Reccomendation
+        driver.implicitly_wait(5)
+        driver.find_element_by_xpath("/html/body/div[1]/section/main/section/div[3]/div[2]/div[1]/a/div").click()
+
+        #Follow People
+        driver.implicitly_wait(7.5)
+        for x in range(1, 11):
+            driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div[2]/div/div/div[" + str(x) + "]/div[3]/button").click()
+            driver.implicitly_wait(random.randint(2,8))
+        time.sleep(1)
+        driver.close()
