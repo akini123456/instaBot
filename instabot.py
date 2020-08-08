fd51447ca3f2bcd4d7850077d28de8599aa488e0
akini123456 <41455205+akini123456@users.noreply.github.com>
2020-08-08 10:11:27 -0400

    Updated Log In

--- instabot.py
+++ instabot.py
@@ -14,7 +14,9 @@ password = "engagestemeb421"
 #URL for Website
 url = "https://www.instagram.com/"
 while forever:
-    time.sleep(180)
+    for x in range(1, 181):
+        time.sleep(1)
+        print(x)
     #Open Browser and Open Up Instragram
     try:
         driver = webdriver.Chrome('C:/Users/aryan/Downloads/chromedriver.exe')
@@ -31,7 +33,7 @@ while forever:
         driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button/div").click() #Log in Button
 
         #Auto-Login Denied
-        driver.implicitly_wait(10)
+        driver.implicitly_wait(15)
         try:
             driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div/div/div/button").click()
         except:
@@ -47,11 +49,23 @@ while forever:
         #See All New Reccomendation
         driver.implicitly_wait(5)
         driver.find_element_by_xpath("/html/body/div[1]/section/main/section/div[3]/div[2]/div[1]/a/div").click()
-
-        #Follow People
-        driver.implicitly_wait(7.5)
-        for x in range(1, 11):
-            driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div[2]/div/div/div[" + str(x) + "]/div[3]/button").click()
-            driver.implicitly_wait(random.randint(8,16))
-        time.sleep(1)
-        driver.close()
+        driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div[2]/div/div/div[1]/div[3]/button").click()
+        try:
+            driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[2]/button[2]").click()
+            print("Instagram found out im a bot")
+            print("Shut down for 10 minutes starts now!")
+            driver.close()
+            for x in range(1, 601):
+                time.sleep(1)
+                print(x)
+        except:
+            #Follow People
+            driver.implicitly_wait(7.5)
+            for x in range(2, 9):
+                try:
+                    driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div[2]/div/div/div[" + str(x) + "]/div[3]/button").click()
+                    driver.implicitly_wait(random.randint(8,16))
+                except:
+                    print("No More Suggestions Available")
+            time.sleep(5)
+            driver.close()
